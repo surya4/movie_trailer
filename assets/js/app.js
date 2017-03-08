d2feb54200b20c43b740a1b20c19948bfb5d089a
SURYA PRAKASH <surya2902@gmail.com>
2017-03-08 11:48:53 +0530

    movie trailer first commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  var searchbox-input = $('.searchbox-input');
+  var searchbox-submit = $('.searchbox-submit');
+
+  searchbox-input.keyup(function(){
+    searchbox-input.
+  });
+
+});
