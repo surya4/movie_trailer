d63c74bb7f7b1cb3e0ffac8b069994c09e98609e
SURYA PRAKASH <surya2902@gmail.com>
2017-03-17 22:36:35 +0530

    new change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
-  var searchbox-input = $('.searchbox-input');
-  var searchbox-submit = $('.searchbox-submit');
 
-  searchbox-input.keyup(function(){
-    searchbox-input.
-  });
+  $.getJSON('data.json', function(data){
+        console.log(data);
+    });
 
 });
