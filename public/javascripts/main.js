a5524b76e185cf431bef044f39761398013df4b7
Surya Singh <surya2902@gmail.com>
2017-05-05 01:52:06 +0530

    removed unsplash

--- public/javascripts/main.js
+++
@@ -1,5 +0,0 @@
-// // var objectGotFromServerSide = <%= whatever %> ;
-//             for(var element in objectGotFromServerSide) {
-//                 var innerHtml = '<div class="col-xs-8 col-md-8 new-box">  <a href="#" class=""><img src="'+element.src+'" alt="trending"><div class="caption"><h6>'+element.name+'</h6><hr></div>  </a></div>';
-//                 $(innerHtml).appendTo($("#boxes"));
-//             }
